23b34d0d61ad53bacd77fa59c8eb0a67ebf3a3a7
Victor Aguilar <victorhugo.webdeveloper@gmail.com>
2019-06-07 08:10:05 -0500

    Terminando funcionalidad de controles

--- js/steps.js
+++ js/steps.js
@@ -4,6 +4,20 @@
 
     const selector = "#contact-form"
 
+    $(".path-step").on("click",(ev)=>{
+        const $current_circle = $(ev.target)
+
+        focus_circle($current_circle)
+
+        const posicion = $current_circle.index(".path-step") + 1
+
+        let $test = $(".step:nth-child("+posicion+")")
+
+        siguiente($test)
+
+        
+    })
+
     $(selector).find(".input").on("change",(ev)=>{
         let $input = $(ev.target)
 
@@ -14,7 +28,6 @@
         } else {
             validar_formulario()
         }
-
 
     })
 
@@ -37,8 +50,22 @@
         $(".step.active").removeClass("active")
         $next_step.addClass("active")
         $next_step.find(".input").focus()
+
+
+        //Coordinar circulos
+        const posicion = ($next_step.index(".step") * 2) + 1
+       
+
+        const $circle = $(".path-step:nth-child("+posicion+")")
+
+        focus_circle($circle)
       
         // $next_input.focus()
+    }
+
+    function focus_circle($circle) {
+        $(".path-step.active").removeClass("active")
+        $circle.addClass("active")
     }
 
 

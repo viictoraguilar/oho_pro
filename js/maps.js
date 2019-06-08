65212b5df10ff365d74bbc236724a20134e5c02a
Victor Aguilar <victorhugo.webdeveloper@gmail.com>
2019-06-08 08:04:55 -0500

    Calculando duracion del viaje

--- js/maps.js
+++ js/maps.js
@@ -15,21 +15,39 @@
         }
     }
 
+    const my_place = {
+        lat: 19.689018,
+        lng: -101.167633      
+    }
+
     google.maps.event.addDomListener(window,"load",()=>{
        const map = new google.maps.Map(
            document.getElementById('map'),
            {
-               center: {
-                lat: 19.689018,
-                lng: -101.167633
-               },
+               center: my_place,
                zoom: 15
            }
        )
 
        UserLocation.get((coords)=>{
-          alert("ya tengo las coordenadas")
-       })
+          //Calcular distancia de la tienda al usuario
+          let origen= new google.maps.LatLng(coords.lat, coords.lng)  //latlng
+          let destino = new google.maps.LatLng(my_place.lat, my_place.lng)
+          let service = new google.maps.DistanceMatrixService()
+          service.getDistanceMatrix({
+              origins: [origen],
+              destinations: [destino],
+              travelMode: google.maps.TravelMode.DRIVING
+          },(response,status)=>{
+            if(status === google.maps.DistanceMatrixStatus.OK){
+                const duration_element = response.rows[0].elements[0]
+                const  duracion_viaje = duration_element.duration.text
+                
+            }
+          })
+        })
+
+        
 
     })
 })()

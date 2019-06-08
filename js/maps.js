89bf38116cd1aa78ca1fab1bd9bd38edce50306b
Victor Aguilar <victorhugo.webdeveloper@gmail.com>
2019-06-08 07:13:34 -0500

    Obteniendo coordenadas del usuario

--- js/maps.js
+++ js/maps.js
@@ -1,4 +1,20 @@
 ;(function(){
+    
+    class UserLocation {
+        static get(callback) {
+            if(navigator.geolocation) {
+               navigator.geolocation.getCurrentPosition((location)=>{
+                   callback({
+                       lat: location.coords.latitude,
+                       lng: location.coords.longitude
+                   })
+               })
+            }else {
+                alert("No pudimos detectar el lugar en el que te encuentras")
+            }
+        }
+    }
+
     google.maps.event.addDomListener(window,"load",()=>{
        const map = new google.maps.Map(
            document.getElementById('map'),
@@ -10,5 +26,10 @@
                zoom: 15
            }
        )
+
+       UserLocation.get((coords)=>{
+          alert("ya tengo las coordenadas")
+       })
+
     })
 })()
